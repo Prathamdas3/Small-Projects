25e884bd0ab7c67a0556ee7746f1efb6bd8827e6
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2023-07-31 21:12:58 +0530

    feat: navbar complete with working darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
-import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  const [mode, setMode] = useState({
+    color: 50,
+    backgroundColor: "#111827",
+  });
+  document.body.style.backgroundColor = mode.backgroundColor;
+  const changeMode = () => {
+    if (mode.color === 50) {
+      setMode({ color: 950, backgroundColor: "#f3f4f6" });
+    } else {
+      setMode({ color: 50, backgroundColor: "#111827" });
+    }
+  };
+
+  return (
+    <>
+      <Navbar mode={mode.color} changeMode={changeMode} />
+    </>
+  );
 }
 
 export default App;
