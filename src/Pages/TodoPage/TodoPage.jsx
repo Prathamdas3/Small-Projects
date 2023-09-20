83e311c4261a308f50b05287bfa284b0f42ba931
Pratham das <prathamdas003@gmail.com>
2023-09-20 15:59:32 +0530

    (feat): todo adding feature is working

--- src/Pages/TodoPage/TodoPage.jsx
+++ src/Pages/TodoPage/TodoPage.jsx
@@ -1,11 +1,24 @@
-import Todos from './component/TodoSection';
+import { useContext, useState, useEffect } from 'react';
+import TodoContext from '../../context/todoApi/todoApiContext';
+import TodoList from './component/TodoSection';
 
 const TodoPage = () => {
+  const todoData = useContext(TodoContext);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [tags, setTags] = useState('');
+  const [isComplete, setIsComplete] = useState(false);
+
+  useEffect(() => {
+    todoData.getData();
+  });
+
   return (
     <div className="overflow-x-auto mt-10 mx-auto container">
       <table className="table">
         {/* head */}
-        <thead>
+        <thead className="mb-2">
           <tr>
             <th>
               <label>
@@ -23,38 +36,48 @@ const TodoPage = () => {
             <th>Title</th>
             <th>Description</th>
             <th>Tags</th>
-            <th></th>
           </tr>
         </thead>
-        <Todos />
+        <TodoList />
       </table>
+      {/* This is the input section */}
       <dialog id="my_modal_1" className="modal">
         <div className="modal-box">
           <h3 className="font-bold text-lg mb-2">Title</h3>
           <input
             type="text"
-            placeholder="Title here"
             className="input input-bordered w-full max-w-[28rem]"
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title here"
           />
           <p className="py-3">Title is required</p>
           <h3 className="font-bold text-lg mb-2">Description</h3>
           <textarea
             className="textarea textarea-bordered min-w-[28rem]"
+            onChange={(e) => setDescription(e.target.value)}
             placeholder="Description here"
-          ></textarea>
+          />
           <p className="py-3">Description is required</p>
           <h3 className="font-bold text-lg mb-2">Tags</h3>
           <input
             type="text"
-            placeholder="Title here"
             className="input input-bordered w-full max-w-[28rem]"
+            onChange={(e) => setTags(e.target.value)}
+            placeholder="Tags here"
           />
           {/* button section */}
           <div className="modal-action ">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
               <button className="btn mr-2">Close</button>
-              <button className="btn btn-success">Save</button>
+              <button
+                className="btn btn-success"
+                onClick={() =>
+                  todoData.addData(title, description, isComplete, tags)
+                }
+              >
+                Save
+              </button>
             </form>
           </div>
         </div>
--- src/context/todoApi/todoApiState.jsx
+++ src/context/todoApi/todoApiState.jsx
@@ -1,63 +1,56 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import todoApiContext from './todoApiContext';
 
 const apiState = (props) => {
   const [allData, setAllData] = useState();
-  // const [title, setTitle] = useState('');
-  // const [description, setDescription] = useState('');
-  // const [isComplete, setIsComplete] = useState(false);
 
-  useEffect(() => {
-    const getData = async () => {
-      const headers = {
-        'Content-Type': 'application/json',
-      };
-      const url = 'http://localhost:5000/todo/';
-      try {
-        const data = await fetch(url, { method: 'GET', headers: headers });
-        const newData = await data.json();
-        setAllData(newData);
-      } catch (error) {
-        console.log(error);
-      }
+  //getting all the todos
+  const getData = async () => {
+    const headers = {
+      'Content-Type': 'application/json',
     };
+    const url = 'http://localhost:5000/todo/';
+    try {
+      const data = await fetch(url, { method: 'GET', headers: headers });
+      const newData = await data.json();
+      setAllData(newData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    getData();
-  }, []);
+  //adding new todos
+  const addData = async (title, description, isComplete, tags) => {
+    const data = {
+      title: title,
+      description: description,
+      tags: tags,
+      is_Complete: isComplete,
+    };
+    const headers = {
+      'content-type': 'application/json',
+    };
+    const body = JSON.stringify(data);
+    const url = 'http://localhost:5000/todo/';
 
-  // useEffect(() => {
-  //   const addData = async () => {
-  //     const data = {
-  //       title: title,
-  //       description: description,
-  //       is_Complete: isComplete,
-  //     };
-  //     const headers = {
-  //       'content-type': 'application/json',
-  //     };
-  //     const body = JSON.stringify(data);
-  //     const url = 'http://localhost:5000/todo/';
+    try {
+      const setData = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: body,
+      });
+      const jsonData = await setData.json();
 
-  //     try {
-  //       const setData = await fetch(url, {
-  //         method: 'POST',
-  //         headers: headers,
-  //         body: body,
-  //       });
-  //       return await setData.json();
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-
-  //   addData();
-  // }, []);
+      setData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const exportData = {
     allData,
-    // setTitle,
-    // setDescription,
-    // setIsComplete,
+    addData,
+    getData,
   };
 
   return (
